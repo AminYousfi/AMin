c1c0402d6240bcdaba81105411bd1e90c0cd3ec4
Amin Yousefi <aminyousefiaaa@gmail.com>
2023-12-14 00:09:04 +0330

    update2

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,21 @@
 import './Home.css'
 
 function Home(){
-    return
+    return(
+        <>
+        <div className='Home  flex flex-row justify-between'>
+            <div className=''>
+                <h1 className='text-9xl mt-24 ms-12  mb-12 font-bold'>Hi ,</h1>
+                <p className='text-6xl ms-12 mb-11'>My Name is <span className='text-9xl ms-5 fontName font-bold text-green-800'>amin</span></p>
+                <button className='btn text-green-600 outline outline-1 p-2 ms-12 hover:bg-green-600 hover:text-white btnDownload hover:outline-green-600 rounded-xl' href='#' download>Donload Resume</button>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+
+        </>
+    )
 }
 
 export default Home
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import './Navigation.css'
 function Navigation(){
     return(
         <>
-        <div className="Nav flex flex-row justify-center align-middle bg-slate-900 fixed top-0 left-0 block ">
+        <div className="Nav flex flex-row justify-center align-middle bg-slate-900 sticky top-0 left-0 block ">
             <div className='navItem text-yellow-50'>Home</div>
             <div className='navItem text-yellow-50'>About Me</div>
             <div className='navItem text-yellow-50'>Skills</div>
