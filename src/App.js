338a1f4bf1cf27e629aa39e3e654430cfa21e825
Amin Yousefi <aminyousefiaaa@gmail.com>
2023-12-14 21:50:27 +0330

    add about Comp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Home from './Components/Home/Home';
 import Navigation from './Components/Navigation/Navigation';
+import About from './Components/About/About';
 
 function App() {
   return(
     <>
     <Navigation />
     <Home />
+    <About />
     </>
   )
 }
