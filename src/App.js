c3c11777c27db7d0dda59a2fbea61dbe6df21714
Amin Yousefi <aminyousefiaaa@gmail.com>
2023-12-15 00:08:40 +0330

    Add Skills

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Components/Home/Home';
 import Navigation from './Components/Navigation/Navigation';
 import About from './Components/About/About';
+import Skills from './Components/Skills/Skills';
 
 function App() {
   return(
@@ -10,6 +11,7 @@ function App() {
     <Navigation />
     <Home />
     <About />
+    <Skills />
     </>
   )
 }
---
+++ src/Components/Skills/Skills.js
@@ -0,0 +1,48 @@
+import './Skills.css'
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3 } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaBootstrap } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { FaSass } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { FaVuejs } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+import { FaGit } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+function skills(){
+    return(
+        <>
+        <div className='skillsContainer bg-white'>
+            <div className="titleSkills flex justify-center items-center ">
+                <h1 className='inline-block font-bold mt-7 text-3xl titleSkillsContent '>Skills</h1>
+            </div>
+
+            
+            <div className="iconArea grid grid-cols-4 gap-12 mt-16">
+                <dev className='flex justify-center align-middle'><FaHtml5 className='text-7xl' title='HTML5' /></dev>
+                <dev className='flex justify-center align-middle'><FaCss3 className='text-7xl' title='Css'/></dev>
+                <dev className='flex justify-center align-middle'><IoLogoJavascript className='text-7xl' title='JavaScript'/></dev>
+                <dev className='flex justify-center align-middle'><FaBootstrap className='text-7xl' title='Bottstrap'/></dev>
+                <dev className='flex justify-center align-middle'><SiTailwindcss className='text-7xl' title='Tailwind'/></dev>
+                <dev className='flex justify-center align-middle'><SiTypescript className='text-7xl' title='TypeScript'/></dev>
+                <dev className='flex justify-center align-middle'><FaSass className='text-7xl' title='Sass'/></dev>
+                <dev className='flex justify-center align-middle'><FaReact className='text-7xl' title='React Js'/></dev>
+                <dev className='flex justify-center align-middle'><FaVuejs className='text-7xl' title='Vue Js'/></dev>
+                <dev className='flex justify-center align-middle'><SiRedux className='text-7xl' title='Redux'/></dev>
+                <dev className='flex justify-center align-middle'><FaGit className='text-7xl' title='Git'/></dev>
+                <dev className='flex justify-center align-middle'><FaGithub className='text-7xl' title='Github'/></dev>
+
+
+            </div>
+            
+        </div>
+        </>
+
+
+    )
+}
+
+export default skills ;
