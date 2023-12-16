d00cf2b7b1215082de664930ded6f3f56e1a7a4e
Amin Yousefi <aminyousefiaaa@gmail.com>
2023-12-16 20:21:51 +0330

    Fix bug Resume

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-
+import Resume from '../../assets/Resume/Resume.pdf' 
 import './Home.css'
 
 function Home(){
@@ -10,7 +10,7 @@ function Home(){
             <div className=''>
                 <h1 className='text-9xl mt-24 ms-12  mb-12 font-bold'>Hi ,</h1>
                 <p className='text-6xl ms-12 mb-11'>My Name is <span className='text-9xl ms-5 fontName font-bold text-green-800'>amin</span></p>
-                <a className='btn text-green-600 outline outline-1 p-2 ms-12 hover:bg-green-600 hover:text-white btnDownload hover:outline-green-600 rounded-xl' href='./../../assests/Resume/Resume.pdf' download='../../assests/Resume/Resume.pdf'>Download Resume</a>
+                <a className='btn text-green-600 outline outline-1 p-2 ms-12 hover:bg-green-600 hover:text-white btnDownload hover:outline-green-600 rounded-xl' href={Resume} download>Download Resume</a>
             </div>
             <div className=' h-full w-1/2 flex justify-center align-middle coffee '>
                 <xmlns version="1.0" encoding="UTF-8"/>
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,12 +1,14 @@
+
 import './Navigation.css'
 function Navigation(){
+
     return(
         <>
         <div className="Nav flex flex-row justify-center align-middle bg-slate-900 sticky top-0 left-0 block ">
             <div className='navItem text-yellow-50'>Home</div>
-            <div className='navItem text-yellow-50'>About Me</div>
+            <div className='navItem text-yellow-50' >About Me</div>
             <div className='navItem text-yellow-50'>Skills</div>
-            <div className='navItem text-yellow-50'>Contact Me</div>
+            <div className='navItem text-yellow-50' >Contact Me</div>
             
         </div>
         
